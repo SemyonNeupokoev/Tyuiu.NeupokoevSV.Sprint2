cf3c7f1f9b07982b7afb47630df4f1db9653bbde
Neupokoev Semyon <semyon_neupokoev@mail.ru>
2025-10-02 16:45:47 +0300

    Add project files.

---
+++ Tyuiu.NeupokoevSV.Sprint2.Task0.V26.Test/DataServiceTest.cs
@@ -0,0 +1,13 @@
+﻿using Tyuiu.NeupokoevSV.Sprint2.Task0.V26.Lib;
+namespace Tyuiu.NeupokoevSV.Sprint2.Task0.V26.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
---
+++ Tyuiu.NeupokoevSV.Sprint2.Task0.V26/Program.cs
@@ -0,0 +1,8 @@
+﻿using Tyuiu.NeupokoevSV.Sprint2.Task0.V26.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
