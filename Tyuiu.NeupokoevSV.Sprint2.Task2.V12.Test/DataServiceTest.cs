3312bc675d2cad1326a59402f15fb24afc262e77
Neupokoev Semyon <semyon_neupokoev@mail.ru>
2025-10-03 18:16:08 +0300

    Task2

---
+++ Tyuiu.NeupokoevSV.Sprint2.Task2.V12.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.NeupokoevSV.Sprint2.Task2.V12.Lib;
+namespace Tyuiu.NeupokoevSV.Sprint2.Task2.V12.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int x = 4;
+            int y = 4;
+            bool res = ds.CheckDotInShadedArea(x, y);
+            bool wait = true;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.NeupokoevSV.Sprint2.Task2.V12/Program.cs
@@ -0,0 +1,26 @@
+﻿using Tyuiu.NeupokoevSV.Sprint2.Task2.V12.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        int x = Convert.ToInt32(Console.ReadLine());
+        int y = Convert.ToInt32(Console.ReadLine());
+
+        bool res = ds.CheckDotInShadedArea(x, y);
+        Console.WriteLine();
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        if (res)
+        {
+            Console.WriteLine("Точка находится в заштрихованной области");
+        }
+        else
+        {
+            Console.WriteLine("Точка находится в заштрихованной области");
+        }
+    }
+}
