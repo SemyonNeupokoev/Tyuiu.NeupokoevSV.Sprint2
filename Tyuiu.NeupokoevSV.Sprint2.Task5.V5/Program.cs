fd530bfc35da5d973f4cf1d3777c435b85eed3aa
Neupokoev Semyon <semyon_neupokoev@mail.ru>
2025-10-05 10:29:35 +0300

    Task5

---
+++ Tyuiu.NeupokoevSV.Sprint2.Task5.V5/Program.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.NeupokoevSV.Sprint2.Task5.V5.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        int x = Convert.ToInt32(Console.ReadLine());
+
+        string res = ds.FindCardValue(x);
+        Console.WriteLine();
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine(res);
+    }
+}
