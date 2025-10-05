4079d90f1a79f16c78e11233b627853c9f6bdbd5
Neupokoev Semyon <semyon_neupokoev@mail.ru>
2025-10-05 11:40:40 +0300

    Task6

---
+++ Tyuiu.NeupokoevSV.Sprint2.Task6.V1.Test/DataServiceTest.cs
@@ -0,0 +1,25 @@
+﻿using Tyuiu.NeupokoevSV.Sprint2.Task6.V1.Lib;
+namespace Tyuiu.NeupokoevSV.Sprint2.Task6.V1.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            Assert.AreEqual(31, ds.FindMonthDaysCount(1));
+            Assert.AreEqual(28, ds.FindMonthDaysCount(2));
+            Assert.AreEqual(31, ds.FindMonthDaysCount(3));
+            Assert.AreEqual(30, ds.FindMonthDaysCount(4));
+            Assert.AreEqual(31, ds.FindMonthDaysCount(5));
+            Assert.AreEqual(30, ds.FindMonthDaysCount(6));
+            Assert.AreEqual(31, ds.FindMonthDaysCount(7));
+            Assert.AreEqual(31, ds.FindMonthDaysCount(8));
+            Assert.AreEqual(30, ds.FindMonthDaysCount(9));
+            Assert.AreEqual(31, ds.FindMonthDaysCount(10));
+            Assert.AreEqual(30, ds.FindMonthDaysCount(11));
+            Assert.AreEqual(31, ds.FindMonthDaysCount(12));
+        }
+    }
+}
---
+++ Tyuiu.NeupokoevSV.Sprint2.Task6.V1/Program.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.NeupokoevSV.Sprint2.Task6.V1.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        int x = Convert.ToInt32(Console.ReadLine());
+
+        int res = ds.FindMonthDaysCount(x);
+        Console.WriteLine();
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine(res);
+    }
+}
